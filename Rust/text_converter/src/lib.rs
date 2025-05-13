54d4c5bfa0c6cccf0f5fbd431f644645445e61e4
Pietro Di Bello <pierodibello@gmail.com>
2025-05-13 08:59:15 +0200

    refactor: move HtmlPagesConverter definition to end of file

--- Rust/text_converter/src/lib.rs
+++ Rust/text_converter/src/lib.rs
@@ -1,7 +1,63 @@
 // src/lib.rs
 use std::fs::File;
 use std::io::{self, BufRead, BufReader, Seek, SeekFrom};
-use std::path::Path;
+
+pub struct HtmlTextConverter {
+    full_filename_with_path: String,
+}
+
+impl HtmlTextConverter {
+    pub fn new(full_filename_with_path: &str) -> Self {
+        Self {
+            full_filename_with_path: full_filename_with_path.to_string(),
+        }
+    }
+
+    pub fn convert_to_html(&self) -> io::Result<String> {
+        let file = File::open(&self.full_filename_with_path)?;
+        let reader = BufReader::new(file);
+        let mut html = String::new();
+
+        for line in reader.lines() {
+            let line = line?;
+            html.push_str(&escape_html(&line));
+            html.push_str("<br />");
+        }
+
+        Ok(html)
+    }
+
+    pub fn get_filename(&self) -> &str {
+        &self.full_filename_with_path
+    }
+}
+
+fn escape_html(input: &str) -> String {
+    input
+        .replace("&", "&amp;")
+        .replace("<", "&lt;")
+        .replace(">", "&gt;")
+        .replace("\"", "&quot;")
+        .replace("'", "&#39;")
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    #[ignore]
+    fn test_html_pages_converter() {
+        let converter = HtmlPagesConverter::new("foo.txt").unwrap();
+        assert_eq!("foo.txt", converter.get_filename());
+    }
+
+    #[test]
+    fn test_html_text_converter() {
+        let converter = HtmlTextConverter::new("foo.txt");
+        assert_eq!("foo.txt", converter.get_filename());
+    }
+}
 
 pub struct HtmlPagesConverter {
     filename: String,
@@ -49,61 +105,5 @@ impl HtmlPagesConverter {
 
     pub fn get_filename(&self) -> &str {
         &self.filename
-    }
-}
-
-pub struct HtmlTextConverter {
-    full_filename_with_path: String,
-}
-
-impl HtmlTextConverter {
-    pub fn new(full_filename_with_path: &str) -> Self {
-        Self {
-            full_filename_with_path: full_filename_with_path.to_string(),
-        }
-    }
-
-    pub fn convert_to_html(&self) -> io::Result<String> {
-        let file = File::open(&self.full_filename_with_path)?;
-        let reader = BufReader::new(file);
-        let mut html = String::new();
-
-        for line in reader.lines() {
-            let line = line?;
-            html.push_str(&escape_html(&line));
-            html.push_str("<br />");
-        }
-
-        Ok(html)
-    }
-
-    pub fn get_filename(&self) -> &str {
-        &self.full_filename_with_path
-    }
-}
-
-fn escape_html(input: &str) -> String {
-    input
-        .replace("&", "&amp;")
-        .replace("<", "&lt;")
-        .replace(">", "&gt;")
-        .replace("\"", "&quot;")
-        .replace("'", "&#39;")
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_html_pages_converter() {
-        let converter = HtmlPagesConverter::new("foo.txt").unwrap();
-        assert_eq!("foo.txt", converter.get_filename());
-    }
-
-    #[test]
-    fn test_html_text_converter() {
-        let converter = HtmlTextConverter::new("foo.txt");
-        assert_eq!("foo.txt", converter.get_filename());
     }
 }

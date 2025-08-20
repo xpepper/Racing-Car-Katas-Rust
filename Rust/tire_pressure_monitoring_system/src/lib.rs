7556a1544389a6b0b43d7a148b59e1a5917b6572
Pietro Di Bello <pierodibello@gmail.com>
2025-08-20 18:34:40 +0200

    refactor: slightly reduce the coupling between Alarm and its sensor component

--- Rust/tire_pressure_monitoring_system/src/lib.rs
+++ Rust/tire_pressure_monitoring_system/src/lib.rs
@@ -9,11 +9,11 @@ pub mod tire_pressure_monitoring_system {
     }
 
     impl Alarm {
-        pub fn new() -> Self {
+        pub fn new(pressure_sensor: RandomPressureSensor) -> Self {
             Alarm {
                 low_pressure_threshold: 17.0,
                 high_pressure_threshold: 21.0,
-                sensor: RandomPressureSensor::new(),
+                sensor: pressure_sensor,
                 alarm_on: false,
             }
         }
@@ -62,11 +62,11 @@ pub mod tire_pressure_monitoring_system {
 
     #[cfg(test)]
     mod tests {
-        use super::{Alarm};
+        use super::{Alarm, RandomPressureSensor};
 
         #[test]
         fn test_alarm() {
-            let mut alarm = Alarm::new();
+            let mut alarm = Alarm::new(RandomPressureSensor::new());
             alarm.check();
             assert_eq!(false, alarm.is_alarm_on());
         }
